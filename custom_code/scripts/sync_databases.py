396ec6e018ff68e93ff220d71fc3111bf12b2499
Craig Pellegrino <craigpellegrino@gmail.com>
2024-10-03 14:54:50 -0400

    Bug fix

--- custom_code/scripts/sync_databases.py
+++ custom_code/scripts/sync_databases.py
@@ -342,10 +342,10 @@ def update_spec(action, db_address=_SNEX2_DB):
                             snex2_id = value.get('snex2_id', '')
                             spec = db_session.query(Datum).filter(and_(Datum.data_type=='spectroscopy', Datum.id==snex2_id)).first()
                             if not spec.data_product_id:
-                                spec.delete()
+                                db_session.delete(spec)
                             else: # Delete the associated DataProduct with the spectrum
                                 data_product_id = spec.data_product_id
-                                spec.delete()
+                                db_session.delete(spec)
                                 db_session.query(Data_Product).filter(Data_Product.id == data_product_id).delete()
                             
                             break
